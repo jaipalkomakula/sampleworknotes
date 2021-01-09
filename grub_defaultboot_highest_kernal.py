7bf872f0a8f74922f51dd05f21828a07e4a675dd
jaipalkomakula <48495380+jaipalkomakula@users.noreply.github.com>
2021-01-09 15:39:10 -0500

    Update grub_defaultboot_highest_kernal.py

--- grub_defaultboot_highest_kernal.py
+++ grub_defaultboot_highest_kernal.py
@@ -1,37 +1,34 @@
 #!/usr/bin/env python
 
-
 import os
 import re
-
-
 
 def main():
  kernel= ()
  regex = re.compile('title CentOS.*')
  regex2 = re.compile('default=[0-9]')
- with open('Metricly_dsoStatus','r') as line:
+ with open('/etc/grub.conf','r') as line:
     for line in line.readlines():
         if regex.match(line):
             kernel = kernel + ((line.split(' ')[2].replace('(', '').replace(')', '').split('-')[1].split('.')[0]),)
         if regex2.match(line):
         	src = line
         	value = int(line.split('=')[1])
-        	
 
- 
+
+
  h = (max(kernel))
  index = kernel.index(h)
  target = ( 'default=' + str(index) + '\n' )
- 
+
  if value == index:
  	exit ()
  else:
- 	with open('Metricly_dsoStatus','r') as f:
+ 	with open('/etc/grub.conf','r') as f:
  		newline=[]
  		for word in f.readlines():
  			newline.append(word.replace(src,target))
- 	with open("Metricly_dsoStatus","w") as f:
+ 	with open("/etc/grub.conf","w") as f:
  		for line in newline:
  			f.writelines(line)
 
